5a7d0320141c5adbe7ce762dc700905e90c92063
leavalos <l.e.avalos1@gmail.com>
2018-04-24 15:23:18 -0300

    se corrige codigo del main

--- main.js
+++ main.js
@@ -35,26 +35,16 @@ function main() {
     break;
 
   case 'addAlbum':
-<<<<<<< HEAD
     if(unqfy.getArtistByName(process.argv[3].toLowerCase()) !== undefined){
       unqfy.addAlbum(process.argv[3].toLowerCase(), {name: process.argv[4].toLowerCase(), year: parseInt( process.argv[5])});
-=======
-    if(unqfy.getArtistByName(process.argv[3]) !== undefined){
-      unqfy.addAlbum(process.argv[3], {name: process.argv[4], year: parseInt(process.argv[5])});
->>>>>>> ce4fa88d558e2d6ff27d5e913f6bf15f29a35e25
     }else{
       console.log('No existe el artista: ' + process.argv[3]);
     }
     break;
 
   case 'addTrack':
-<<<<<<< HEAD
     if(unqfy.getAlbumByName(process.argv[3].toLowerCase()) !== undefined){
-      unqfy.addTrack(process.argv[3].toLowerCase(), {name: process.argv[4].toLowerCase(), duration: parseInt(process.argv[5]), genre: process.argv[6]});
-=======
-    if(unqfy.getAlbumByName(process.argv[3]) !== undefined){
-      unqfy.addTrack(process.argv[3], {name: process.argv[4], duration: parseInt(process.argv[5]), genre: process.argv[6]});
->>>>>>> ce4fa88d558e2d6ff27d5e913f6bf15f29a35e25
+      unqfy.addTrack(process.argv[3].toLowerCase(), {name: process.argv[4].toLowerCase(), duration: parseInt(process.argv[5]), genre: process.argv[6].toLowerCase()});
     }else{
       console.log('No existe el album: ' + process.argv[3]);
     }
@@ -66,19 +56,19 @@ function main() {
     break;
 
   case 'getArtistByName':
-    console.log(unqfy.getArtistByName(process.argv[3]).toLowerCase());
+    console.log(unqfy.getArtistByName(process.argv[3].toLowerCase()));
     break;
 
   case 'getAlbumByName':
-    console.log(unqfy.getAlbumByName(process.argv[3]).toLowerCase());
+    console.log(unqfy.getAlbumByName(process.argv[3].toLowerCase()));
     break;
 
   case 'getTrackByName':
-    console.log(unqfy.getTrackByName(process.argv[3]).toLowerCase());
+    console.log(unqfy.getTrackByName(process.argv[3].toLowerCase()));
     break;
   
   case 'getPlaylistByName':
-    console.log(unqfy.getPlaylistByName(process.argv[3]).toLowerCase());
+    console.log(unqfy.getPlaylistByName(process.argv[3].toLowerCase()));
     break;
   
   case 'getTracksMatchingGenres':
@@ -91,15 +81,15 @@ function main() {
     break;
 
   case 'getTracksMatchingParcialName':
-    console.log(unqfy.getTracksMatchingParcialName(process.argv[3]).toLowerCase());
+    console.log(unqfy.getTracksMatchingParcialName(process.argv[3].toLowerCase()));
     break;
 
   case 'getAlbumsMatchingParcialName':
-    console.log(unqfy.getAlbumsMatchingParcialName(process.argv[3]).toLowerCase());
+    console.log(unqfy.getAlbumsMatchingParcialName(process.argv[3].toLowerCase()));
     break;
 
   case 'getArtistsMatchingParcialName':
-    console.log(unqfy.getArtistsMatchingParcialName(process.argv[3]).toLowerCase());
+    console.log(unqfy.getArtistsMatchingParcialName(process.argv[3].toLowerCase()));
     break;
   }
 
